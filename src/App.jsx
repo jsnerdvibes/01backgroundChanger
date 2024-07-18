f45883ea0a0195e816b8b5d5866c04c48ebd3f83
JS Nerd <shaikhdanish1710@gmail.com>
2024-07-18 20:37:25 +0530

    Updated with Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 function App() {
 
   const [color,setColor]=useState("grey")
@@ -6,13 +7,13 @@ function App() {
 
   return (
     <>
-      <div style={{backgroundColor:color}} className='buttonArea' >
-        <button className='btn' onClick={()=>{setColor("red")}}>Red</button>
-        <button className='btn' onClick={()=>{setColor("blue")}}>Blue</button>
-        <button className='btn' onClick={()=>{setColor("green")}}>Green</button>
-        <button className='btn' onClick={()=>{setColor("Orange")}}>Orange</button>
-        <button className='btn' onClick={()=>{setColor("pink")}}>pink</button>
-        <button className='btn' onClick={()=>{setColor("Black")}}>Black</button>
+      <div style={{backgroundColor:color}} className='h-96 flex justify-center	items-end	' >
+        <button className=' text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' onClick={()=>{setColor("red")}}>Red</button>
+        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' onClick={()=>{setColor("blue")}}>Blue</button>
+        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' onClick={()=>{setColor("green")}}>Green</button>
+        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' onClick={()=>{setColor("Orange")}}>Orange</button>
+        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' onClick={()=>{setColor("pink")}}>pink</button>
+        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800' onClick={()=>{setColor("Black")}}>Black</button>
       </div>
     </>
   )
